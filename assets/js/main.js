f7021904c436f28d546394fddb27cd822fc38e90
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-29 14:29:49 -0400

    iconos funcionan

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 
+
+    $("img.lazy").lazyload({
+    	effect: 'fadeIn',
+    });
+
 	json.forEach(function(e){
 		var img = e.id+1;
-		$('.contenedor').append("<div class='elemento'><img src='dist/img/"+ img+ ".jpg'><p class='title'>"+ e.title+ "</p><p>"+ e.description+"</p></div>");
+		$('.contenedor').append("<div class='elemento'><img class='lazy' src='dist/img/"+ img+ ".jpg'><i class='fa fa-thumb-tack icono' aria-hidden='true'></i><i class='fa fa-check icono' aria-hidden='true'></i><p class='title'>"+ e.title+ "</p><br><p class='description'>"+ e.description+"</p><p class='user'>"+e.username+"   "+"#"+e.hashtag+"</p></div>");
 	})
+
 
 		var $container = $('.contenedor'); 
 			$container.imagesLoaded( function() {
@@ -14,8 +20,9 @@ $(document).ready(function() {
 	$(".open").click(function(event) {
 		var img = $(this).attr("src");
 		var muestraIMG = "<div id='apear_image_div' onClick='closeImage'></div>";
-		muestraIMG = muestraIMG.concat("<img id='mi_img' src='dest/img/"+ 1+ "'.jpg/>");
+		muestraIMG = muestraIMG.concat("<img id='mi_img' data-original='dest/img/"+ 1+ "'.jpg/>");
 		$('body').append(muestraIMG);
 		
 	});
 });
+
